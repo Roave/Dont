74c0ec39b7481a1d9351a263f8d4691ff748a032
Marco Pivetta <ocramius@gmail.com>
2016-10-11 05:09:41 +0200

    Spec for a `TypeError` (for when unexpected stuff is passed around)

---
+++ tests/DontTest/Exception/TypeErrorTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DontTest;
+
+use Dont\Exception\ExceptionInterface;
+use Dont\Exception\TypeError;
+
+/**
+ * @covers \Dont\Exception\TypeError
+ */
+final class TypeErrorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider nonObjectProvider
+     *
+     * @param mixed $nonObject
+     *
+     * @return void
+     */
+    public function testFromNonObject($nonObject)
+    {
+        $exception = TypeError::fromNonObject($nonObject);
+
+        self::assertInstanceOf(TypeError::class, $exception);
+        self::assertInstanceOf(\InvalidArgumentException::class, $exception);
+        self::assertInstanceOf(ExceptionInterface::class, $exception);
+
+        self::assertSame(
+            'Expected object to be given, found ' . gettype($nonObject) . ' instead',
+            $exception->getMessage()
+        );
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public function nonObjectProvider() : array
+    {
+        return [
+            [null],
+            [true],
+            [123],
+            [12.3],
+            ['foo'],
+            [[]],
+            [STDERR],
+        ];
+    }
+}
