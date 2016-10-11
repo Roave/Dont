b88648ba6ce9e00f84d4fa17b89fbce846ae4e8d
Marco Pivetta <ocramius@gmail.com>
2016-10-11 04:58:07 +0200

    Implementing `NonSerializableObject` as per specification

---
+++ src/Dont/Exception/NonSerializableObject.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dont\Exception;
+
+use LogicException;
+
+class NonSerializableObject extends LogicException implements ExceptionInterface
+{
+    /**
+     * @param object $object
+     *
+     * @return NonSerializableObject
+     */
+    public static function fromAttemptedSerialization($object) : self
+    {
+        $template = <<<'ERROR'
+The given object %s#%s is not designed to be serialized, yet serialization was attempted.
+
+This error is raised because the author of %s didn't design it to be serializable, nor can
+guarantee that it will function correctly after serialization, nor can guarantee that all
+its internal components are serializable.
+
+Please do not serialize %s instances.
+ERROR;
+
+        $className = get_class($object);
+
+        return new self(sprintf(
+            $template,
+            $className,
+            spl_object_hash($object),
+            $className,
+            $className
+        ));
+    }
+}
