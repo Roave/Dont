e71c7a8cf168bdf4c20d62bbb7b247068a30dbb2
Marco Pivetta <ocramius@gmail.com>
2016-10-11 05:35:55 +0200

    Implementing `NonSerializableObject` as per spec

---
+++ src/Dont/Serialize.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dont;
+
+use Dont\Exception\NonSerializableObject;
+use Dont\Exception\TypeError;
+
+trait Serialize
+{
+    /**
+     * @return void
+     *
+     * @throws NonSerializableObject
+     * @throws TypeError
+     */
+    public function __sleep()
+    {
+        throw NonSerializableObject::fromAttemptedSerialization($this);
+    }
+}
