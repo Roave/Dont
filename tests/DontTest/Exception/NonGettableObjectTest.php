7a969b3a6c58b6c89a45a2a8076a0483d53b3d86
James Titcumb <james@asgrim.com>
2018-10-25 17:01:38 +0100

    Added tests missing from the other PR

---
+++ tests/DontTest/Exception/NonGettableObjectTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DontTest\Exception;
+
+use Dont\Exception\ExceptionInterface;
+use Dont\Exception\NonGettableObject;
+use Dont\Exception\TypeError;
+use LogicException;
+use stdClass;
+
+/**
+ * @covers \Dont\Exception\NonGettableObject
+ */
+final class NonGettableObjectTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider objectProvider
+     *
+     * @param object $object
+     */
+    public function testFromAttemptedGet($object) : void
+    {
+        $exception = NonGettableObject::fromAttemptedGet($object, 'propertyName');
+
+        self::assertInstanceOf(NonGettableObject::class, $exception);
+        self::assertInstanceOf(LogicException::class, $exception);
+        self::assertInstanceOf(ExceptionInterface::class, $exception);
+
+        $expected = 'The given object ' . get_class($object)
+            . '#' . spl_object_hash($object) . " is not designed to allow any undefined or inaccessible properties to be read from.\n\n"
+            . 'You tried to access a property called "propertyName".' . "\n\n"
+            . 'Perhaps you made a typo in the property name, or tried to access an inaccessible property?';
+
+        self::assertSame($expected, $exception->getMessage());
+    }
+
+    /**
+     * @return object[][]
+     */
+    public function objectProvider() : array
+    {
+        return [
+            [new stdClass()],
+            [$this],
+        ];
+    }
+
+    /**
+     * @dataProvider nonObjectProvider
+     *
+     * @param mixed $nonObject
+     */
+    public function testWillThrowOnNonObject($nonObject) : void
+    {
+        $this->expectException(TypeError::class);
+
+        NonGettableObject::fromAttemptedGet($nonObject, 'propertyName');
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public function nonObjectProvider() : array
+    {
+        return [
+            [null],
+            [true],
+            [123],
+            [12.3],
+            ['foo'],
+            [[]],
+            [STDERR],
+        ];
+    }
+}
